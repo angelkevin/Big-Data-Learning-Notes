c78ccc2942a9a30f87534acc39fb305bf8e32fa8
angelkevin <2215408861@qq.com>
2022-07-25 23:08:14 +0800

    upup

---
+++ zkw1/src/main/java/Leetcode/Solution20.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+
+import java.util.Stack;
+
+public class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character>stack = new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c=='(')stack.push(')');
+            else if(c=='[')stack.push(']');
+            else if(c=='{')stack.push('}');
+            else if(stack.isEmpty()||c!=stack.pop())return false;
+        }
+        return stack.isEmpty();
+    }
+}
+
