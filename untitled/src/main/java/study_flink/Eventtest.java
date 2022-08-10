9eb266fb899ea9b69dfb15a422444f540e3cbaa6
angelkevin <2215408861@qq.com>
2022-08-10 22:18:40 +0800

    Changes

--- untitled/src/main/java/study_flink/Eventtest.java
+++ untitled/src/main/java/study_flink/Eventtest.java
@@ -1,7 +1,11 @@
 package study_flink;
 
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.util.ArrayList;
 
@@ -11,11 +15,21 @@ public class Eventtest {
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         executionEnvironment.setParallelism(1);
         ArrayList<Event> events = new ArrayList<Event>();
-        events.add(new Event("asd", "asd", 456L));
+//        events.add(new Event("asd", "asd", 456L));
 //        从集合中读取数据
-        DataStreamSource<Event> eventDataStreamSource = executionEnvironment.fromCollection(events);
+//        DataStreamSource<Event> eventDataStreamSource = executionEnvironment.fromCollection(events);
+//
+//        eventDataStreamSource.print();
+        DataStreamSource<String> line = executionEnvironment.readTextFile("input/user");
+        SingleOutputStreamOperator<Tuple1<String>> res = line.flatMap((String data, Collector<Tuple1<String>> out) ->
+        {
+            String[] strings = data.split(",");
+            Event event = new Event(strings[0], strings[1], Long.parseLong(strings[2]));
+            String rr = event.toString();
+            out.collect(Tuple1.of(rr));
+        }).returns(Types.TUPLE(Types.STRING));
 
-        eventDataStreamSource.print();
+        res.print();
         executionEnvironment.execute();
     }
 }
