7b264f84d386b2a4ea6a5103f2377e1114033203
angelkevin <2215408861@qq.com>
2022-08-10 21:33:37 +0800

    Changes

--- untitled/src/main/java/study_flink/Eventtest.java
+++ untitled/src/main/java/study_flink/Eventtest.java
@@ -9,11 +9,8 @@ public class Eventtest {
     public static void main(String[] args) throws Exception {
 //        创建执行环境
         StreamExecutionEnvironment executionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
-
         executionEnvironment.setParallelism(1);
-
         ArrayList<Event> events = new ArrayList<Event>();
-
         events.add(new Event("asd", "asd", 456L));
 //        从集合中读取数据
         DataStreamSource<Event> eventDataStreamSource = executionEnvironment.fromCollection(events);
